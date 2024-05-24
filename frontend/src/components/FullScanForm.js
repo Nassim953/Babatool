bade4b59e57bc9cb51946408bcde7d65dfdc1d91
Nassim953 <nassim.babaci@supdevinci-edu.fr>
2024-05-24 09:37:09 +0200

    modifications

--- frontend/src/components/FullScanForm.js
+++ frontend/src/components/FullScanForm.js
@@ -27,7 +27,7 @@ function FullScanForm() {
     }
     console.log(data);
     try {
-      const response = await fetch('http://127.0.0.1:5001/scan', {
+      const response = await fetch('http://127.0.0.1:5000/scan', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
